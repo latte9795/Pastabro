833e3dc09b3d9ddb7ab3d7cae74213d533edbd3a
latte9795 <106307848+latte9795@users.noreply.github.com>
2026-04-05 15:55:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'pastabro-v1';
+const URLS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
